1ab668abd931427945c2469ab117b3909721dbbb
Niladri Sekhar Rout <niladri30012003@gmail.com>
2024-12-21 00:44:12 +0530

    push

--- src/Tabrizi/Chauffage/Chauffage.jsx
+++ src/Tabrizi/Chauffage/Chauffage.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Background from "./image/background-Image.jpeg";
 import Title_Home from "./image/chaufage.png";
+import Title_Home1 from "./image1.png";
+import Title_Home2 from "./image.png";
 import Contact from "../Contact/Contact";
 import "./Chauffage.css";
 import HeaderChauffage from "./HeaderChauffage.jsx";
@@ -113,37 +115,43 @@ const Chauffage = () => {
             </div>
 
             <div className="background">
-              <div className="container">
-                <div className="mr-40">
-                  <div className="relative">
-                    {/* Image with red border */}
-                    <img
-                      className="title w-full object-cover"
-                      src={Title_Home}
-                      alt="Titre"
-                    />
-                    {/* Text overlay for larger screens */}
-                    <div className="absolute inset-0 hidden sm:flex items-center justify-center">
-                      <p className="text-white text-left text-lg sm:text-xl md:text-2xl lg:text-3xl p-4 ml-5">
-                        Découvrez nos solutions de chauffage écologiques avec
-                        des pompes à chaleur performantes pour une énergie verte
-                        et durable. Optimisez votre confort tout en réduisant
-                        votre empreinte carbone.
-                      </p>
-                    </div>
-                    {/* Text below the image for small screens */}
-                    <div className="sm:hidden p-4">
-                      <p className="text-left text-gray-700 text-sm">
-                        Découvrez nos solutions de chauffage écologiques avec
-                        des pompes à chaleur performantes pour une énergie verte
-                        et durable. Optimisez votre confort tout en réduisant
-                        votre empreinte carbone.
-                      </p>
-                    </div>
+            <div className="container">
+              <div className="md:mr-40">
+                <div className="relative">
+                  {/* Image */}
+                  <img
+                  className="title w-full object-cover h-auto hidden sm:block"
+                  src={Title_Home}
+                  alt="Titre"
+                />
+              
+                {/* Image for small screens */}
+                <img
+                  className="title w-full object-cover h-auto sm:hidden"
+                  src={Title_Home1}
+                  alt="Titre"
+                />
+                 {/**  <img
+                  className="title w-full object-cover h-auto sm:hidden"
+                  src={Title_Home1}
+                  alt="Titre"
+                />*/}
+                  {/* Text overlay */}
+                  <div className="custom-margin absolute inset-0 flex flex-col items-center justify-center  bg-opacity-50 px-4 sm:mt-[50px] md:mt-[50px]">
+                  <h2 className="text-3xl sm:hidden text-white" style={{fontWeight:"500px", fontFamily:"Rubik"}}>CHAUFFAGE</h2>
+                  <div className="ml-10">
+                  <p className="text-black text-center text-xs sm:text-sm md:text-base lg:text-lg p-4 leading-normal sm:leading-relaxed sm:mt-5 ml-0 sm:ml-9">
+                  Découvrez nos solutions de chauffage écologiques avec des pompes à
+                  chaleur performantes pour une énergie verte et durable. Optimisez
+                  votre confort tout en réduisant votre empreinte carbone.
+                </p>
+                </div>
                   </div>
                 </div>
               </div>
             </div>
+          </div>
+          
           </div>
 
           <section id="chhauffage-con">
@@ -197,3 +205,4 @@ const Chauffage = () => {
 };
 
 export default Chauffage;
+
