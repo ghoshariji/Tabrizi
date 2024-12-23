3298328b1d6bfb54f9f11b818461980e74cfefb6
ghoshariji <144580430+ghoshariji@users.noreply.github.com>
2024-12-23 17:03:35 +0530

    push

--- src/Tabrizi/Chauffage/Chauffage.jsx
+++ src/Tabrizi/Chauffage/Chauffage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Background from "./image/background-Image.jpeg";
-import Title_Home from "./image/chaufage.png";
+import Title_Home from "./image/chaufage.svg";
 import Title_Home1 from "./image1.png";
 import Title_Home2 from "./image.png";
 import Contact from "../Contact/Contact";
@@ -108,52 +108,37 @@ const Chauffage = () => {
             )}
           </button>
 
-          <div className="home">
-            <div className="background-img">
-              <img src={Background} alt="Fond" />
+          <div className="home relative">
+            <div className="background-img relative sm:h-[250vh] md:h-4/5 lg:h-2/5">
+              <img
+                src={Background}
+                alt="Fond"
+                className="w-full h-full object-cover"
+              />
               <div className="absolute inset-0 bg-black opacity-50"></div>
             </div>
 
-            <div className="background">
-              <div className="container">
-                <div className="md:mr-40">
-                  <div className="relative">
-                    {/* Image */}
-                    <img
-                      className="title w-full object-cover h-auto hidden sm:block"
-                      src={Title_Home}
-                      alt="Titre"
-                    />
-
-                    {/* Image for small screens */}
-                    <img
-                      className="title w-full object-cover h-auto sm:hidden"
-                      src={Title_Home1}
-                      alt="Titre"
-                    />
-                    {/**  <img
-                  className="title w-full object-cover h-auto sm:hidden"
-                  src={Title_Home1}
+            {/* Second div placed on top of the first div */}
+            <div className="absolute inset-0 flex items-center justify-center w-full sm:h-[150vh] md:h-[70vh] lg:h-[60vh] px-4 lg:px-12 lg:mt-20 z-10">
+              <div className="relative w-full sm:w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] mx-auto lg:my-8">
+                <img
+                  className="w-full sm:h-[70vh] md:h-auto lg:h-auto object-cover"
+                  src={Title_Home}
                   alt="Titre"
-                />*/}
-                    {/* Text overlay */}
-                    <div className="custom-margin absolute inset-0 flex flex-col items-center justify-center  bg-opacity-50 px-4 sm:mt-[50px] md:mt-[50px]">
-                      <h2
-                        className="text-3xl sm:hidden text-white"
-                        style={{ fontWeight: "500px", fontFamily: "Rubik" }}
-                      >
-                        CHAUFFAGE
-                      </h2>
-                      <div className="ml-10">
-                        <p className="text-black text-center text-xs sm:text-sm md:text-base lg:text-lg p-4 leading-normal sm:leading-relaxed sm:mt-5 ml-0 sm:ml-9">
-                          Découvrez nos solutions de chauffage écologiques avec
-                          des pompes à chaleur performantes pour une énergie
-                          verte et durable. Optimisez votre confort tout en
-                          réduisant votre empreinte carbone.
-                        </p>
-                      </div>
-                    </div>
-                  </div>
+                />
+                {/* Text over the image */}
+                <div className="absolute inset-0 flex flex-col items-center justify-center text-white px-4">
+                  <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl font-semibold text-center">
+                    CHAUFFAGE
+                  </h1>
+                  <p className="text-xs sm:text-sm md:text-lg lg:text-lg text-center sm:px-6 lg:px-12 leading-relaxed">
+                    Découvrez nos solutions de chauffage écologiques
+                    <br className="hidden sm:block" /> avec des pompes à chaleur
+                    performantes
+                    <br /> pour une énergie verte et durable.
+                    <br className="hidden sm:block" /> Optimisez votre confort
+                    tout en réduisant votre empreinte carbone.
+                  </p>
                 </div>
               </div>
             </div>
@@ -161,9 +146,7 @@ const Chauffage = () => {
 
           <section id="chhauffage-con">
             <div className="chauffage-container">
-              <div
-                className="section"
-              >
+              <div className="section">
                 <h2 className="mt-5">ÉNERGIE VERTE</h2>
 
                 <div className="item">
@@ -176,7 +159,6 @@ const Chauffage = () => {
                       flexDirection: "column",
                     }}
                     onClick={() => contactFunc("Pompe à chaleur air/eau")}
-
                   >
                     <img src={img1} />
                     <h3
@@ -201,7 +183,6 @@ const Chauffage = () => {
                       flexDirection: "column",
                     }}
                     onClick={() => contactFunc("Pompe à chaleur air/air")}
-
                   >
                     <img src={img2} />
                     <h3
@@ -217,9 +198,7 @@ const Chauffage = () => {
                 </div>
               </div>
 
-              <div
-                className="section"
-              >
+              <div className="section">
                 <h2 className="mt-5">ÉNERGIE FOSSILE</h2>
 
                 <div className="item">
@@ -232,7 +211,6 @@ const Chauffage = () => {
                       flexDirection: "column",
                     }}
                     onClick={() => contactFunc("Chauffage à mazout")}
-
                   >
                     <img src={img3} />
                     <h3
@@ -257,7 +235,6 @@ const Chauffage = () => {
                       flexDirection: "column",
                     }}
                     onClick={() => contactFunc("Chauffage à gaz")}
-
                   >
                     <img src={img4} />
                     <h3
